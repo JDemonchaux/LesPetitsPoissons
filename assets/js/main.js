cc4f44d4d2fb6679f39f3c2499ae0fd36b9544b5
Jerome Demonchaux <jerome.demonchaux@gmail.com>
2016-03-16 20:59:31 +0100

    ryacasting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,44 @@
-const GAME = new Phaser.Game(1280, 720, Phaser.AUTO, '', {preload: preload, create: create, update: update});
+const GAME = new Phaser.Game(1280, 720, Phaser.AUTO, '', {
+    preload: preload,
+    create: create,
+    update: update,
+    render: render
+});
 
+
+var map;
+var layer;
+var bitmap;
 var fish;
-var food = [];
+var food;
 var group;
 var cursors;
 var currentSpeed = 0;
 var counter = 1;
 var socket;
+var line;
 
 function preload() {
     GAME.load.image('background', 'assets/images/bg/aquarium.jpg');
     GAME.load.image('food', 'assets/images/food/food.png');
     GAME.load.image('fish1', 'assets/images/fishes/fish1.png');
 
-    socket = io("http://10.6.43.1:5000/socket.io");
-    socket.on('canal test', function (msg) {
-        var input = msg.split(";");
-        console.log(input);
-    });
+    //socket = io("http://10.6.43.1:5000/socket.io");
+    //socket.on('canal test', function (msg) {
+    //    var input = msg.split(";");
+    //    console.log(input);
+    //});
 
 }
 
 
 function create() {
     GAME.physics.startSystem(Phaser.Physics.ARCADE);
-    GAME.add.image(0, 0, 'background');
+    //GAME.add.image(0, 0, 'background');
+
+    map = GAME.add.tilemap();
+    layer = map.createBlankLayer('Layer 1', 40, 30, 32, 32);
+    layer.debug = true;
 
     //add the fucking god damn fish
     fish = GAME.add.sprite(GAME.world.centerX, GAME.world.centerY, "fish1");
@@ -35,9 +49,18 @@ function create() {
     fish.body.collideWorldBounds = true;
     fish.body.immovable = false;
 
-    // Groupe pour le manger
+    // Groupe pour la nourriture
     group = GAME.add.physicsGroup();
 
+    // Add the god damn food
+    addFood();
+
+
+    // trash
+    bitmap = GAME.add.bitmapData(this.game.width, this.game.height);
+    bitmap.context.fillStyle = 'rgb(255, 255, 255)';
+    bitmap.context.strokeStyle = 'rgb(255, 255, 255)';
+    GAME.add.image(0, 0, bitmap);
     cursors = GAME.input.keyboard.createCursorKeys();
 }
 
@@ -66,13 +89,31 @@ function update() {
     }
 
 
+    // Clear the line before redraw
+    bitmap.context.clearRect(0, 0, this.game.width, this.game.height);
+
+    for (i = 0; i < group.children.length; i++) {
+        var ray = new Phaser.Line(fish.x, fish.y, group.children[i].world.x, group.children[i].world.y);
+
+        bitmap.context.beginPath();
+        bitmap.context.moveTo(fish.x, fish.y);
+        bitmap.context.lineTo(group.children[i].world.x + 8, group.children[i].world.y + 8);
+        bitmap.context.stroke();
+    }
+    ;
+
+    bitmap.dirty = true;
+
+}
+
+function render() {
+
 }
 
 
 function collisionHandler(fish, food) {
-    console.log("Vous avez mangé : " + counter + " nourriture");
-    counter++;
     food.kill();
+    group.remove(food);
 }
 
 function processHandler(fish, food) {
@@ -82,11 +123,15 @@ function processHandler(fish, food) {
 
 function addFood() {
     for (i = 0; i < 20; i++) {
-        var c = group.create(GAME.world.randomX, GAME.world.randomY, "food");
+        var x = GAME.world.randomX;
+        var y = GAME.world.randomY;
+
+        var c = group.create(x, y, "food");
         c.scale.setTo(0.5, 0.5);
-        c.body.mass = -100;
+        //c.body.mass = -100;
     }
 }
+
 
 function handleFish(input) {
     if (input[1] == 1.0) {
